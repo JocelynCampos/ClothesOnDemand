3b47f8c3a905061688245f1b855c5cef923500d9
Jocelyn <aracely.campos@hotmail.com>
2025-03-04 18:10:26 +0100

    Added PropertyChangeSupport and added Main method

--- src/main/java/controllers/TrafficController.java
+++ src/main/java/controllers/TrafficController.java
@@ -4,7 +4,7 @@ import java.beans.PropertyChangeListener;
 
 public class TrafficController implements PropertyChangeListener {
      public void propertyChange(PropertyChangeEvent event) {
-         System.out.println("Traffic control detected in :" + get);
+         System.out.println("Traffic control detected in: " + event.getPropertyName() + ": " + event.getNewValue());
      }
 
 }
